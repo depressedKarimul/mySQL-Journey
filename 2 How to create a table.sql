059495b559e69f31df457ba0e221841f2475a705
karimul-hassan <karimul.hassan09@gmail.com>
2024-09-02 19:25:28 +0600

    I learned how to create, modify, rename, and delete tables and columns in MySQL, as well as how to retrieve data from a table.

---
+++ 2 How to create a table.sql
@@ -0,0 +1,58 @@
+-- CREATE DATABASE myDB;
+
+-- USE myDB;
+
+-- CREATE TABLE employees(
+
+-- employee_id INT,
+-- first_name VARCHAR(50),
+-- last_name VARCHAR(50),
+-- hourly_pay DECIMAL(5,2),
+-- hire_date DATE
+
+-- );
+
+-- SELECT * FROM employees;
+
+-- RENAME TABLE employees TO workers;
+-- RENAME TABLE workers TO employees;
+
+-- DROP TABLE employees;
+
+-- ALTER TABLE employees 
+-- ADD phone_number VARCHAR(15);
+
+
+-- SELECT * FROM employees;
+
+-- ALTER TABLE employees 
+-- RENAME COLUMN phone_number to email;
+
+-- SELECT * FROM employees;
+
+
+-- ALTER TABLE employees
+-- MODIFY COLUMN email VARCHAR(100);
+
+
+
+-- ALTER TABLE employees
+-- MODIFY email VARCHAR(100)
+-- AFTER last_name;
+
+
+-- SELECT * FROM employees;
+
+
+-- ALTER TABLE employees
+-- MODIFY email VARCHAR(100)
+-- FIRST;
+
+-- SELECT * FROM employees;
+
+
+
+-- ALTER TABLE employees
+-- DROP COLUMN email;
+
+SELECT * FROM employees;
