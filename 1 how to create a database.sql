a84c0d3f4e68db372f0bacfd09a6fddfaaebd6eb
karimul-hassan <karimul.hassan09@gmail.com>
2024-09-02 18:54:14 +0600

    learn how to create and drop a database

---
+++ 1 how to create a database.sql
@@ -0,0 +1,23 @@
+-- How to create database
+-- CREATE DATABASE myDB;
+
+
+-- how to use database
+-- USE myDB;
+
+-- How to delete database 
+-- DROP DATABASE myDB;
+
+
+-- This command makes the database mydb read-only, meaning no changes (like adding, updating, or deleting data) can be made to it. It can only be viewed or queried.
+-- ALTER DATABASE myDB READ ONLY =1;
+
+-- DROP DATABASE myDB;
+
+
+-- This command changes the database mydb back to read/write mode, allowing you to make changes like adding, updating, or deleting data again.
+-- ALTER DATABASE mydb READ ONLY =0;
+
+
+-- DROP DATABASE myDB;
+
